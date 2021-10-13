726b034ff383464db0fb47f0f058afabbb3c78c2
verdenkaa <nikitapahomov97@gmail.com>
2021-10-13 18:43:40 +0300

    first
    Добавил первичный экран, через ui
    Кнопки пока не работают

---
+++ main.py
@@ -0,0 +1,21 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('facer.ui', self)
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = MyWidget()
+    form.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
