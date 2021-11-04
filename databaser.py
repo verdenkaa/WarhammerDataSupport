aee6dfc3b8dea384d96734b2b035bb619374c71f
Legion2185 <83009365+lUniverseGameplay@users.noreply.github.com>
2021-11-04 12:32:08 +0300

    correct

--- databaser.py
+++ databaser.py
@@ -20,7 +20,7 @@ class Dater(QMainWindow):
         else:
             # Иначе подключаем готовую считаную и отсортированую базу в списке кортежей
             self.datasheets = stddb
-            
+
         self.forback = forback
         self.table_creator()
         self.pushButton.clicked.connect(self.back)
@@ -42,13 +42,13 @@ class Dater(QMainWindow):
         self.button_list = [QPushButton('Добавить в список моей армии', self) for i in range(len(self.datasheets))]
         for i in range(len(self.datasheets)):
             for j in range(len(self.datasheets[i])):
-                if self.datasheets[i][j] == self.datasheets[i][-2]:  #проверка на 2 с конца элемент, там должно быть изображение
+                if self.datasheets[i][j] == self.datasheets[i][14]:  #проверка на 2 с конца элемент, там должно быть изображение
                     image = QLabel(self)
                     pix = QPixmap('Unit_image/' + str(self.datasheets[i][j])) #Из списка с данными sql мы берем имя картинки
-                    _size = QSize(200, 100) 
+                    _size = QSize(200, 100)
                     image.setPixmap(pix.scaled(_size, Qt.KeepAspectRatio)) #подгоняем размер
                     self.tables.setCellWidget(i, j, image) #вставляем widget в таблицу
-                elif self.datasheets[i][j] == self.datasheets[i][-1]:  #проверка на 1 с конца элемент, там должна быть кнопка
+                elif self.datasheets[i][j] == self.datasheets[i][15]:  #проверка на 1 с конца элемент, там должна быть кнопка
                     self.tables.setCellWidget(i, j, self.button_list[i]) #вставляем widget в таблицу
                     self.conn(self.button_list[i], self.datasheets[i][0], self.datasheets[i][2]) #вызываем функцию conn в которой назначим кнопке определённое действие
                 else:
@@ -59,7 +59,7 @@ class Dater(QMainWindow):
 
     def conn(self, a, unit_name, rase):
         a.clicked.connect(lambda: self.add_unit(unit_name, rase)) #если этого не сделать, передаваться значения будут только из последней кнопки
-    
+
     def add_unit(self, unit_name, rase):
         c = 'army_list/' + str(rase) + '.txt' #создаем путь текстового файла с определённым названием
         d = open(c, mode = "a+") #открываем файл
@@ -70,13 +70,13 @@ class Dater(QMainWindow):
 
     def clear_army_list(self):
         f, ok_pressed = QInputDialog.getItem(
-            self, "Выберите файл для очистки", "Выберите файл для очистки", 
+            self, "Выберите файл для очистки", "Выберите файл для очистки",
             ("Astartes.txt", "Necrons.txt", "Bubonic.txt", "Orcs.txt"), 1, False) #Диалоговое окно для выбора файла
         c = 'army_list/' + f
         d = open(c, mode = "w")
         d.write('') #очищаем файл
         d.close
-    
+
     def back(self):
         self.hide()
         self.forback.setWindowTitle('Warhammer Data Support')
